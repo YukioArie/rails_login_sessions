5e91e08502c33132081def0f776795f62365dfcc
Yukio Arie <yukioarie@gmail.com>
2022-05-27 21:22:33 -0300

    add new routes and verify_login action from login controller

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,4 +2,20 @@ class LoginController < ApplicationController
   def index
     console
   end
+  def verify_login
+    user_data = Rails.cache.read(params[:email])
+    if user_data
+      user_data_password = user_data.split('@@').last
+      if user_data_password == params[:password]
+        user_data_username = user_data.split('@@').first
+        cookies[:userId] = SecureRandom.uuid
+        Rails.cache.write(cookies[:userId], "#{user_data_username}@@#{params[:email]}")
+        redirect_to home_index_path
+      else
+      redirect_to login_index_path, notice: 'Senha inválida'
+      end
+    else
+      redirect_to login_index_path, notice: 'Usuário não encontrado'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'login/index' 
+  get 'login/index'
+  post 'login/verify_login', to: 'login#verify_login'
+
+  get 'register/index'
+  get 'home/index' 
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
