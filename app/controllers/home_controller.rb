2ba5cf4fe2fb77def70885b5e6f37d2af6a55ba5
Yukio Arie <yukioarie@gmail.com>
2022-05-27 22:00:30 -0300

    add method to get user_data through cookie userId key

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!
   def index
+    user_data = Rails.cache.read(cookies[:userId]).split('@@')
+    @username = user_data.first
+    @email = user_data.last
+
   end
+  def authenticate_user!
+    unless cookies[:userId]
+      redirect_to login_index_path
+    end
+    
+  end
+  
 end
